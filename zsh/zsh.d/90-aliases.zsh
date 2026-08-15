6354ae330772aa2576cb2bca462dec42e63f75a7
Dustin Liddick <dustin.liddick@synegen.com>
2026-08-15 19:38:21 -0400

    Restore short db tunnel aliases
    start-db, stop-db and list-db existed only in the old ~/.dotfiles checkout
    and were never carried over in the sync. Keep the longer -tunnel forms too
    so both work.

--- zsh/zsh.d/90-aliases.zsh
+++ zsh/zsh.d/90-aliases.zsh
@@ -1,3 +1,8 @@
 alias start-db-tunnel='db-tunnel-start'
 alias stop-db-tunnel='db-tunnel-stop'
 alias list-db-tunnel='print -rl -- ${(k)DB_TUNNELS}'
+
+# Short forms, carried over from the old ~/.dotfiles checkout.
+alias start-db='db-tunnel-start'
+alias stop-db='db-tunnel-stop'
+alias list-db='print -rl -- ${(k)DB_TUNNELS}'
